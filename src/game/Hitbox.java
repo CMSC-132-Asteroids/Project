a30e55ddf43d6583121810726fda57e545512d6f
Declan <60301669+qwertydelle@users.noreply.github.com>
2025-03-10 10:49:08 -0400

    Hitbox class added

---
+++ src/game/Hitbox.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Hitbox {
+	Polygon shape;
+	public Hitbox(Point[] points, Point offset, double rotation) {
+		this.shape = new Polygon(points, offset, rotation);
+	}
+}
